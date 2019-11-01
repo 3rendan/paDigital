5b735ae19dea40eb3fce22c378e327cecf804e36
Brendan <brendan.ryan@pm.me>
2019-11-01 16:19:56 -0400

    saved old version of collections by mistake, also updated README.md

--- themes/pvdAth/collections/browse.php
+++ themes/pvdAth/collections/browse.php
@@ -5,18 +5,19 @@
 <div class="container"> 
     <div class="row">
         <div class="col-sm-11 col-sm-offset-1">
-            <h3><?php echo 'Browse collections'; ?></h3> 
+            <h3><?php echo 'Browse all collections'; ?></h3> 
         </div>       
     </div>       
-    <?php foreach (loop('collections') as $collection): ?>
     <div class="flex-container">
-                    <?php if ($collectionImage = record_image('collection', 'square_thumbnail')): ?>
-                        <?php echo link_to_collection($collectionImage, array('class' => 'image')); ?>
-                    <?php endif; ?>
-                    <h3><?php echo link_to_collection(); ?></h3>
-                <?php fire_plugin_hook('public_collections_browse_each', array('view' => $this, 'collection' => $collection)); ?>
-        </div>
-    <?php endforeach; ?>
+        <?php 
+        foreach (loop('collections') as $collection) {
+            echo "<div class=\"browse-items\">" . link_to_collection($collectionImage, 'square_thumbnail'); 
+            echo "<h5>" . link_to_collection(metadata('collection', array('Dublin Core', 'Title')), array('class'=>'permalink')) . "</h5></div>"; 
+        }
+        ?>
+            
+        <?php fire_plugin_hook('public_collections_browse_each', array('view' => $this, 'collection' => $collection)); ?>
+    </div>
       </div>
     <div class="row">
         <?php echo pagination_links(); ?>   
