7cb42c74debc1dac1e63276d162ac457bcd500f0
Brendan <brendan.ryan@pm.me>
2019-11-03 20:23:38 -0500

    worked on making about and index look the same

--- themes/pvdAth/index.php
+++ themes/pvdAth/index.php
@@ -73,15 +73,17 @@
 <!-- END CAROUSEL -->
 <!-- ABOUT -->
 
-  
-  <div class="about-section mobile-div" style="padding-top: 2em;">
+<div class="col-sm-offset-1 col-sm-10">
+  <div class="paragraph">
     <p>The Providence Athenæum possesses an eclectic art collection of nearly 150 art objects, which range from a delicate ivory miniature to a massive cabinet styled after an Egyptian temple. These artworks - either donated by Athenæum members or purchased by subscription - track developments in art and art collecting from the institution’s founding in 1836 to the present. Together they provide a narrative of the Athenæum’s cultural significance to Providence and the region.</p>
 
 
     <p>The art collection can be browsed by item, collection, or by keyword in the search box.  Each of the objects is accompanied by a downloadable image and a text box that includes the catalog information, a brief description, and a tab for additional resources.</p> 
 
     <p>The digital archive of the art collection has been made possible by a grant from the Herman H. Rose Civic, Cultural and Media Access Fund.</p>
+    <div class="row text-center">
     <a href="http://provath.org" target="_blank"><img src="/uploads/athena.jpg" alt="The Providence Athenæum" style="width:150px;text-align: center;padding-top: 2em;"></a>   
+    </div>
   </div>
 </div>
 
